9aa49616bbbf57005cc7fe2e15533f0e67bdc727
PaulBrR <paalbrisenoro@ittepic.edu.mx>
2019-09-13 11:03:49 -0500

    Se subio el inici

---
+++ app/src/main/java/paulbriseno/ittepi/edu/mx/tpdm_u1_practica2_pualbriseno/Main4Activity.kt
@@ -0,0 +1,12 @@
+package paulbriseno.ittepi.edu.mx.tpdm_u1_practica2_pualbriseno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
